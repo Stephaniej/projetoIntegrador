b7fafa972291e06b5b66c9d68edbda5ca286dc7d
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2019-05-04 13:48:32 -0300

    Add files via upload
    footer separado, index com "include".

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Torabit • Monitoramento digital<br>
+        &copy; Copyright 2019 • Todos os direitos reservados</p>
+    <img src="imgs/imagens_cortadas/torabit.png" width="24%" height="24%">
+</footer>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" media="screen" href="main.css">
         <link rel="stylesheet" type="text/css" media="screen" href="header.css">
-
+        <link rel="stylesheet" type="text/css" media="screen" href="footer.css">
     </head>
 
     <body>
@@ -85,11 +85,9 @@
                 </div>
             </div>
         </div>
-        <footer>
-            <p>Torabit • Monitoramento digital<br>
-                &copy; Copyright 2019 • Todos os direitos reservados</p>
-            <img src="imgs/imagens_cortadas/torabit.png">
-        </footer>
+        
+        <?php include("footer.php");?>
+        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
         </script>
