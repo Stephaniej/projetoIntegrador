060bb39758289effb01e39aa54bdc40a70b1567c
allan souki <allan.souki@hotmail.com>
2019-05-05 19:04:06 -0300

    conf carrosel

---
+++ carrosel.js
@@ -0,0 +1,166 @@
+let img1 = document.querySelector("#link1" );
+
+ 
+
+img1.addEventListener("click", function(){
+
+    window.location.href = "http://www.devmedia.com.br/";
+
+    });
+
+ 
+
+    let img2 = document.querySelector(" #link2  " );
+
+ 
+
+img2.addEventListener("click", function(){
+
+    window.location.href = "https://www.youtube.com/?gl=BR&hl=pt";
+
+    });
+
+ 
+
+    let img3 = document.querySelector(" #link3  " );
+
+ 
+
+    img3.addEventListener("click", function(){
+
+        window.location.href = "http://www.devmedia.com.br/";
+
+        });
+
+ 
+
+        let img4 = document.querySelector("#link4 " );
+
+ 
+
+        img4.addEventListener("click", function(){
+
+            window.location.href = "http://www.devmedia.com.br/";
+
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            let img5 = document.querySelector("#link5" );
+
+ 
+
+img5.addEventListener("click", function(){
+
+    window.location.href = "http://www.devmedia.com.br/";
+
+    });
+
+ 
+
+    let img6 = document.querySelector(" #link6 " );
+
+ 
+
+img6.addEventListener("click", function(){
+
+    window.location.href = "https://www.youtube.com/?gl=BR&hl=pt";
+
+    });
+
+ 
+
+    let img7 = document.querySelector(" #link7  " );
+
+ 
+
+    img7.addEventListener("click", function(){
+
+        window.location.href = "http://www.devmedia.com.br/";
+
+        });
+
+ 
+
+        let img8 = document.querySelector("#link8 " );
+
+ 
+
+        img8.addEventListener("click", function(){
+
+            window.location.href = "http://www.devmedia.com.br/";
+
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            let img9 = document.querySelector("#link9" );
+
+ 
+
+            img9.addEventListener("click", function(){
+            
+                window.location.href = "http://www.devmedia.com.br/";
+            
+                });
+            
+             
+            
+                let img10 = document.querySelector(" #link10 " );
+            
+             
+            
+            img10.addEventListener("click", function(){
+            
+                window.location.href = "https://www.youtube.com/?gl=BR&hl=pt";
+            
+                });
+            
+             
+            
+                let img11 = document.querySelector(" #link11  " );
+            
+             
+            
+                img11.addEventListener("click", function(){
+            
+                    window.location.href = "http://www.devmedia.com.br/";
+            
+                    });
+            
+             
+            
+                    let img12 = document.querySelector("#link12 " );
+            
+             
+            
+                    img12.addEventListener("click", function(){
+            
+                        window.location.href = "http://www.devmedia.com.br/";
+            
+                        });
--- index.php
+++ index.php
@@ -11,16 +11,25 @@
         <link rel="stylesheet" type="text/css" media="screen" href="main.css">
         <link rel="stylesheet" type="text/css" media="screen" href="header.css">
         <link rel="stylesheet" type="text/css" media="screen" href="footer.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="carrossel.css.css">
     </head>
 
     <body>
         <?php include("header.php");?>
+       
         <section class="home-content">
       <div class="image-banner">
-        <h1>PLANEJE E GERENCIE UM ,<br> MONITORAMENTO EM REDES SOCIAIS</h1>
+         <h1>PLANEJE E GERENCIE UM <br> MONITORAMENTO EM REDES SOCIAIS</h1> 
         <button>Download ebook</button>
+       
       </div>
+      <div class="top"> <?php include("carrossel.php");?> </div>
     </section>
+
+
+
+
+    
         <div class="container">
             <div class="row">
                 <div class="col-lg-3">
@@ -88,6 +97,9 @@
         
         <?php include("footer.php");?>
         
+       
+
+        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
         </script>
@@ -97,6 +109,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
             integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
         </script>
+        <script type="text/javascirpt" src="carrosel.js"></script>
     </body>
 
 </html>
