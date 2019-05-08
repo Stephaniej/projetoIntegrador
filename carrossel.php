91b9a3d52de139d51eb62c1224b6138374e2d49f
Stephanie Jorge <stephanie.jorge@gmail.com>
2019-05-08 08:43:07 -0300

    merge antes do samuca

--- carrossel.php
+++ carrossel.php
@@ -1,217 +1,241 @@
 <!DOCTYPE html>
 
-    <html lang="pt">
+<html lang="pt">
 
-    <head>
+  <head>
 
-      <title>Bootstrap Example</title>
-      <link rel="stylesheet" href="carrossel.css">
-      <meta charset="utf-8">
+    <title>Bootstrap Example</title>
+    <link rel="stylesheet" href="carrossel.css">
+    <meta charset="utf-8">
 
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-
-      
-      
-
-    </head>
-
-    <body>
-
-   
-<header>
-                <div class="container">
-
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                      <li data-target="#myCarousel" data-slide-to="1"></li>
-                      <li data-target="#myCarousel" data-slide-to="2"></li>
-                    </ol>
-                 
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
-                      <div class="item active">
-                            <div class="holder">
-
- 
-
-                                    <div id="link1"  class="imagem"   data-title="legenda # 1" >
-                   
-                                      <img   class="radius"    src="imgs/imagens_cortadas/img1.png" alt=""> <button class="botao" >&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                                       </div>
-                   
-                                   
-                                   
-                                       <div  id="link2" class="imagem"   data-title="legenda # 1">
-                   
-                                      <img class="radius"    src="imgs/imagens_cortadas/img2.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                   
-                                    </div>
-                   
-                                    <div id="link3"  class="imagem"   data-title="legenda # 1">
-                   
-                                      <img class="radius"    src="imgs/imagens_cortadas/img3.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                   
-                                       
-                   
-                                    </div>
-                   
-                                 
-                   
-                                    <div id="link4" class="imagem" data-title="legenda # 2">
-                   
-                                      <img class="radius"    src="imgs/imagens_cortadas/img4.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                   
-                                    </div>
-                   
-                                 
-                   
-                                  </div>
-                        <div class="carousel-caption">
-                         
-                        </div>
-                      </div>
-                 
-                      <div class="item">
-                            <div class="holder">
-
- 
-
-                                    <div  id="link5" class="imagem"   data-title="legenda # 1">
-                   
-                                      <img  class="radius"    src="imgs/imagens_cortadas/img1.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                                       </div>
-                   
-                                   
-                                   
-                                       <div  id="link6" class="imagem"   data-title="legenda # 1">
-                   
-                                      <img class="radius"    src="imgs/imagens_cortadas/img2.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                   
-                                    </div>
-                   
-                                    <div  id="link7" class="imagem"   data-title="legenda # 1">
-                   
-                                      <img class="radius"    src="imgs/imagens_cortadas/img3.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                   
-                                       
-                   
-                                    </div>
-                   
-                                 
-                   
-                                    <div id="link8" class="imagem" data-title="legenda # 2">
-                   
-                                      <img class="radius"    src="imgs/imagens_cortadas/img4.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                   
-                                    </div>
-                   
-                                 
-                   
-                                  </div>
-                        <div class="carousel-caption">
-                         
-                        </div>
-                      </div>
-                 
-                      <div class="item">
-                            <div class="holder">
-
- 
-
-                                    <div id="link9" class="imagem"   data-title="legenda # 1">
-                   
-                                      <img  class="radius"    src="imgs/imagens_cortadas/img1.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                                       </div>
-                   
-                                   
-                                   
-                                       <div  id="link10" class="imagem"   data-title="legenda # 1">
-                   
-                                      <img class="radius"    src="imgs/imagens_cortadas/img2.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                   
-                                    </div>
-                   
-                                    <div  id="link11" class="imagem"   data-title="legenda # 1">
-                   
-                                      <img class="radius"    src="imgs/imagens_cortadas/img3.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                   
-                                       
-                   
-                                    </div>
-                   
-                                 
-                   
-                                    <div id="link12" class="imagem" data-title="legenda # 2">
-                   
-                                      <img class="radius"    src="imgs/imagens_cortadas/img4.png" alt=""> <button class="botao">&nbsp; Compre&nbsp;&nbsp; </button> <h6   class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
-                   
-                                        <p  class="pacote"> ou 12 x 67 para pacote anual </p>
-                   
-                                    </div>
-                   
-                                 
-                   
-                                  </div>
-                        <div class="carousel-caption">
-                         
-                        </div>
-                      </div>
-                    </div>
-                 
-                    <!-- Left and right controls -->
-                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                      <span class="glyphicon glyphicon-chevron-left"></span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                      <span class="glyphicon glyphicon-chevron-right"></span>
-                      <span class="sr-only">Next</span>
-                    </a>
-                  </div>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
 
-   </div>
- </header>
+
+  </head>
+
+  <body>
 
 
- 
+    <header>
+      <div class="container">
 
- 
- <script type="text/javascript" src="carrosel.js"></script>
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+          </ol>
 
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+            <div class="item active">
+              <div class="holder">
 
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-   
 
-   
 
-    </body>
+                <div id="link1" class="imagem" data-title="legenda # 1">
 
-    </html>
+                  <img class="radius" src="imgs/imagens_cortadas/img1.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+                </div>
+
+
+
+                <div id="link2" class="imagem" data-title="legenda # 1">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img2.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+
+                </div>
+
+                <div id="link3" class="imagem" data-title="legenda # 1">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img3.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+
+
+
+                </div>
+
+
+
+                <div id="link4" class="imagem" data-title="legenda # 2">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img4.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+
+                </div>
+
+
+
+              </div>
+              <div class="carousel-caption">
+
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="holder">
+
+
+
+                <div id="link5" class="imagem" data-title="legenda # 1">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img1.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+                </div>
+
+
+
+                <div id="link6" class="imagem" data-title="legenda # 1">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img2.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+
+                </div>
+
+                <div id="link7" class="imagem" data-title="legenda # 1">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img3.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+
+
+
+                </div>
+
+
+
+                <div id="link8" class="imagem" data-title="legenda # 2">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img4.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+
+                </div>
+
+
+
+              </div>
+              <div class="carousel-caption">
+
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="holder">
+
+
+
+                <div id="link9" class="imagem" data-title="legenda # 1">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img1.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+                </div>
+
+
+
+                <div id="link10" class="imagem" data-title="legenda # 1">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img2.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+
+                </div>
+
+                <div id="link11" class="imagem" data-title="legenda # 1">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img3.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+
+
+
+                </div>
+
+
+
+                <div id="link12" class="imagem" data-title="legenda # 2">
+
+                  <img class="radius" src="imgs/imagens_cortadas/img4.png" alt=""> <button class="botao">&nbsp;
+                    Compre&nbsp;&nbsp; </button>
+                  <h6 class="valor"> <strong class="diminuir"> R$</strong>79/mês </h6>
+
+                  <p class="pacote"> ou 12 x 67 para pacote anual </p>
+
+                </div>
+
+
+
+              </div>
+              <div class="carousel-caption">
+
+              </div>
+            </div>
+          </div>
+
+          <!-- Left and right controls -->
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+
+
+
+      </div>
+    </header>
+
+
+
+
+
+    <script type="text/javascript" src="carrosel.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+
+
+
+  </body>
+
+</html>
