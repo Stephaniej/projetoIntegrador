68d4092e4b2418148563ba802b95b66ef2218150
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2019-05-09 13:09:09 -0300

    alteração na estrutura

--- header.php
+++ header.php
@@ -1,29 +1,38 @@
-    <header class="cabecalho">
-        <div class="cabecalho1">
-            <a href=""><img href="" class="torabit-shop" src="imgs/imagens_cortadas/torabit_shop.png" width="160"></a>
-            <div id="divBusca">
-                <input class="campoBuscar" type="text" id="txtBusca" placeholder="Buscar..." />
-                <a href=""><img src="imgs/imagens_cortadas/busca_2.png" id="btnBusca" alt="Buscar" width="50" /></a>
-            </div>
-            <div class="list-head">
-                <ul class="list-head1">
-                    <li><a href="">Cadastre-se |</a></li>
-                    <li><a href="">Login |</a></li>
-                </ul>
 
-                <a href=""><img class="carrinho" src="imgs/imagens_cortadas/carrinho.png" alt="torabit-shop"
-                        width="50"></a>
-            </div>
+<header class="navbar navbar-expand-sm navbar-light">
+
+            <!-- Logo -->            
+    <img src="imgs/imagens_cortadas/torabit_shop.png" width="18%" height="18%">
+
+            <!-- Menu Hamburguer -->
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-target">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+            <!-- navegação (botão responsivo) -->
+    <div class="collapse navbar-collapse" id="nav-target">
+            
+       <div class="input-group"> <!-- Barra de pesquisa -->
+        <input class="form-control" type="text" placeholder="Pesquisar">
+          <div class="input-group-append">
+            <button type="button" class="btn search"><p>busca</p></button>
         </div>
-        <div class="cabecalho2">
-            <div class="main-menu">
-                <ul class="list-head2">
-                    <li><a href="">Sobre o Torabit</a></li>
-                    <li><a href="">Categorias</a></li>
-                    <li><a href="">Full Service</a></li>
-                    <li><a href="">Area do Cliente</a></li>
-                    <li><a href="">Contato</a></li>
-                </ul>
-            </div>
-        </div>
-    </header>
+    </div>
+       
+      <ul class="navbar-nav">   <!-- Itens -->
+        <li class="nav-item"><a href="" class="nav-link"><p class="letra">Cadastre-se |</p></a></li>
+        <li class="nav-item"><a href="" class="nav-link"><p class="letra">Login |</p></a></li>
+        <li class="nav-item"><a href="" class="nav-link" id="compras"><p>compras</p></a></li> 
+       </ul>
+    </div>
+</header>
+
+<nav id="nav2">
+      
+        <span><a href="">SOBRE O TORABIT</a></span>
+        <span><a href="">CATEGORIAS</a></span>
+        <span><a href="">FULL SERVICE</a></span>
+        <span><a href="">ÁREA DO CLIENTE</a></span>
+        <span><a href="">CONTATO</a></span>
+
+</nav>
--- index.php
+++ index.php
@@ -16,94 +16,82 @@
 
     <body>
         <?php include("header.php");?>
+
     <section id="conteudo">
-        <section class="home-content">
-            <div class="image-banner">
-                <h1>PLANEJE E GERENCIE UM <br> MONITORAMENTO EM REDES SOCIAIS</h1>
-                <button>Download ebook</button>
-                <?php include("carrossel.php");?>
 
-            </div>
-        </section>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="card" style="width: 100%;">
-                        <img src="imgs/imagens_cortadas/menu1.png" class="card-img-top" alt="conteudo_produto_1">
-                        <div class="card-body">
-                            <h5 class="card-title">Ranking de posts</h5>
-                            <p class="card-text">Compare os posts de suas
-                                marcas de interesse com
-                                seus pares.
-                                Veja os posts que
-                                mais engajam e mais.
-                            </p>
-                            <a href="#" class="btn btn-primary">SAIBA MAIS</a>
-                        </div>
-                    </div>
-                </div>
+        <section>   <!-- Seção para o ebook -->
+            <div id="ebook">
+
+              <span class="pfrase">PLANEJE E GERENCIE UM</span> <br>
+              <span>MONITORAMENTO EM REDES SOCIAIS</span>
+               <div><button type="button" class="btn">Download Ebook</button></div>
+            </div>    <!-- Fim da seção do ebook -->
+           
+
+             <!--   <?php //include("carrossel.php");?> --> 
+                 <!-- Tirei o carrossel acima -->
+
+        </section>  <!-- Fim da sessão para o ebook -->
+
+         <section id="area-carrossel">
+             
+          <!-- Carrosel aqui ? -->
+
+         </section>
 
 
-                <div class="col-lg-3">
-                    <div class="card" style="width: 100%;">
-                        <img src="imgs/imagens_cortadas/menu2.png" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Desempenho de Perfis</h5>
-                            <p class="card-text">Compare perfis
-                                de suas marcas de
-                                com seus
-                                concorrentes e pares.
-                                São 11 plataformas diferentes.
-                            </p>
-                            <a href="#" class="btn btn-primary">SAIBA MAIS</a>
-                        </div>
-                    </div>
-                </div>
+  <section id="alta">    <!-- Seção do "Em alta" -->
+    <h5>EM ALTA</h5>
+  <div class="row">
 
-                <div class="col-lg-3">
-                    <div class="card" style="width: 100%;">
-                        <img src="imgs/imagens_cortadas/menu3.png" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Rastrear Links</h5>
-                            <p class="card-text">Acompanhe os compartilhamentos de
-                                cada notícia em rede e a
-                                vida útil de cada link.
-                            </p>
-                            <a href="#" class="btn btn-primary">SAIBA MAIS</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="card" style="width: 100%;">
-                        <img src="imgs/imagens_cortadas/menu4.png" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Trends</h5>
-                            <p class="card-text">Acompanhe o movimento
-                                em toda a rede.
-                                O que mais falam, o que
-                                mais buscam e o que
-                                mais leem.</p>
-                            <a href="#" class="btn btn-primary">SAIBA MAIS</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr>
-         <?php include("footer.php");?>
-        </div>
-        </section>
+    <div class="col-md-3">
+    <img class="card-img-top" src="imgs/imagens_cortadas/menu1.png">
+      <div class="card-body">
+        <h4 class="card-title">Ranking de posts</h4>
+        <p>Compare os posts de suas marcas de interesse com seus concorrentes e pares. Veja os posts que mais engajam e mais.</p>
+        <a href="" class="btn btn-primary">Saiba mais</a>
+      </div>
+     </div>
+    
 
+    <div class="col-md-3">
+    <img class="card-img-top" src="imgs/imagens_cortadas/menu2.png">
+      <div class="card-body">
+        <h4 class="card-title">Desempenho de Perfis</h4>
+        <p>Compare perfis de suas marcas de com seus concorrentes e pares
+        São 11 plataformas diferentes.</p>
+        <a href="" class="btn btn-primary">Saiba mais</a>
+      </div>
+     </div>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-        </script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-        </script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-        </script>
-        <script type="text/javascirpt" src="carrosel.js"></script>
+    <div class="col-md-3">
+    <img class="card-img-top" src="imgs/imagens_cortadas/menu2.png">
+      <div class="card-body">
+        <h4 class="card-title">Rastrear Links</h4>
+        <p>Acompanhe os compartilhamentos de cada notícia em rede e a
+         vida útil de cada link.</p>
+        <a href="" class="btn btn-primary">Saiba mais</a>
+      </div>
+     </div>
+
+    <div class="col-md-3">
+    <img class="card-img-top" src="imgs/imagens_cortadas/menu2.png">
+      <div class="card-body">
+        <h4 class="card-title">Trends</h4>
+        <p>Acompanhe o movimento em toda a rede. O que mais falam, o que
+           mais buscam e o que mais leem.</p>
+        <a href="" class="btn btn-primary">Saiba mais</a>
+      </div>
+     </div>
+    </div>
+    <hr>
+    <?php include("footer.php");?>
+    </section>
+   </section>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascirpt" src="carrosel.js"></script>
     </body>
-
 </html>
