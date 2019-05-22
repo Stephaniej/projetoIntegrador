064f64508923cf28843d1b60e9a34f6655de0333
Stephanie Jorge <stephanie.jorge@gmail.com>
2019-05-22 17:13:01 -0300

    Links nos botões de cadastro, area do cliente e no logo do header, ajuste no header

--- header.php
+++ header.php
@@ -1,9 +1,17 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>TORABiT</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="header.css" />
+</head>
 <header class="navbar navbar-expand-sm navbar-light">
 
             <!-- Logo -->            
-    <img src="imgs/imagens_cortadas/torabit_shop.png" width="18%" height="18%">
-
+            <div class="logo-content">
+	         <a href="index.php"><img src="imgs/imagens_cortadas/torabit_shop.png"alt="logo do site"></a>
+      </div>
             <!-- Menu Hamburguer -->
     <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-target">
         <span class="navbar-toggler-icon"></span>
@@ -20,7 +28,7 @@
     </div>
        
       <ul class="navbar-nav">   <!-- Itens -->
-        <li class="nav-item"><a href="" class="nav-link"><p class="letra">Cadastre-se |</p></a></li>
+        <li class="nav-item"><a href="cadastro.php" class="nav-link"><p class="letra">Cadastre-se |</p></a></li>
         <li class="nav-item"><a href="" class="nav-link"><p class="letra">Login |</p></a></li>
         <li class="nav-item"><a href="" class="nav-link" id="compras"><p>compras</p></a></li> 
        </ul>
@@ -32,7 +40,7 @@
         <span><a href="">SOBRE O TORABIT</a></span>
         <span><a href="">CATEGORIAS</a></span>
         <span><a href="">FULL SERVICE</a></span>
-        <span><a href="">ÁREA DO CLIENTE</a></span>
+        <span><a href="configuracoes.php">ÁREA DO CLIENTE</a></span>
         <span><a href="">CONTATO</a></span>
 
 </nav>
