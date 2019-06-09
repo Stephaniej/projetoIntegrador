bfe6a70e381c1d5abcaee65b7e11b0ffd55cec1f
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2019-06-09 15:45:18 -0300

    header update

--- header.php
+++ header.php
@@ -1,35 +1,121 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>TORABiT</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="header.css" />
-</head>
+<header>
+    <nav class="navbar navbar-expand-sm">
+
+      <!-- Logo -->            
+      <a href="" class="navbar-brand">
+        <img src="imgs/imagens_cortadas/torabit_shop.png" width="200">
+      </a>
+
+      <!-- Menu Hamburguer -->
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-target2">
+        <div class="btnH topob"></div>
+        <div class="btnH"></div>
+        <div class="btnH"></div>
+         <!-- <span class="navbar-toggler-icon"></span> -->
+      </button>
+
+      <!-- navegação -->
+      <div class="collapse navbar-collapse" id="nav-target2">
+
+       <div class="input-group"> 
+        <input class="form-control" type="text" placeholder="Pesquisar">
+          <div class="input-group-append">
+            <button type="button" class="btn search"><p>busca</p></button>
+        </div>
+       </div>
+
+       <ul class="navbar-nav">
+          <li class="nav-item link"><a href="" class="nav-link">Cadastre-se</a></li>
+          <li class="nav-item link"><a href="" class="nav-link">Login</a></li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+             <img src="imgs/imagens_cortadas/carrinho.png" width="30">    
+            </a>
+          </li>
+        </ul>
+
+       </div>
+      </nav>
+</header>
+
+<nav id="nav2">
+  <div class="container">
+    <div class="navb">
+     
+     <span><a href="">SOBRE O TORABIT</a></span>
+     <span><a href="">CATEGORIAS</a></span>
+     <span><a href="">FULL SERVICE</a></span>
+     <span><a href="">ÁREA DO CLIENTE</a></span>
+     <span><a href="">CONTATO</a></span>
+      
+    </div>  
+  </div>
+</nav>
+
+<!-- 
+<header>        
+      <nav class="navbar navbar-expand-md navbar-light navbar-branco">
+
+       <div class="container">
+        <a href="index.html" class="navbar-brand">
+          <img src="imgs/imagens_cortadas/torabit_shop.png" width="160">
+        </a>
+       </div>
+
+      <div class="input-group"> 
+        <input class="form-control" type="text" placeholder="Pesquisar">
+          <div class="input-group-append">
+            <button type="button" class="btn search"><p>busca</p></button>
+        </div>
+      </div>
+
+       <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-principal">
+         <i class="fas fa-bars text-blue"></i>
+       </button>
+
+       <div class="collapse navbar-collapse" id="nav-principal">
+         <ul class="navbar-nav">
+           <li class="nav-item">
+             <a href="" class="nav-link">Cadastre-se |</a>
+            </li>
+
+            <li class="nav-item">
+             <a href="" class="nav-link">Login |</a>
+            </li>
+
+            <li class="nav-item">
+             <a href="" class="nav-link"><img src="imgs/imagens_cortadas/carrinho.png" width="32"></a>
+            </li>
+         </ul>
+       </div>
+      </nav>
+    </header>   -->
+
+
+<!--
 <header class="navbar navbar-expand-sm navbar-light">
 
-            <!-- Logo -->            
-            <div class="logo-content">
-	         <a href="index.php"><img src="imgs/imagens_cortadas/torabit_shop.png"alt="logo do site"></a>
-      </div>
-            <!-- Menu Hamburguer -->
+                     
+    <img src="imgs/imagens_cortadas/torabit_shop.png" width="18%" height="18%">
+
+            Menu Hamburguer 
     <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-target">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-            <!-- navegação (botão responsivo) -->
+        navegação (botão responsivo)
     <div class="collapse navbar-collapse" id="nav-target">
             
-       <div class="input-group"> <!-- Barra de pesquisa -->
+       <div class="input-group">  Barra de pesquisa 
         <input class="form-control" type="text" placeholder="Pesquisar">
           <div class="input-group-append">
             <button type="button" class="btn search"><p>busca</p></button>
         </div>
     </div>
        
-      <ul class="navbar-nav">   <!-- Itens -->
-        <li class="nav-item"><a href="cadastro.php" class="nav-link"><p class="letra">Cadastre-se |</p></a></li>
-        <li class="nav-item"><a href="form.php" class="nav-link"><p class="letra">Login |</p></a></li>
+      <ul class="navbar-nav">   Itens 
+        <li class="nav-item"><a href="" class="nav-link"><p class="letra">Cadastre-se |</p></a></li>
+        <li class="nav-item"><a href="" class="nav-link"><p class="letra">Login |</p></a></li>
         <li class="nav-item"><a href="" class="nav-link" id="compras"><p>compras</p></a></li> 
        </ul>
     </div>
@@ -37,10 +123,10 @@
 
 <nav id="nav2">
       
-        <span><a href="sobre.php">SOBRE O TORABIT</a></span>
+        <span><a href="">SOBRE O TORABIT</a></span>
         <span><a href="">CATEGORIAS</a></span>
         <span><a href="">FULL SERVICE</a></span>
-        <span><a href="configuracoes.php">ÁREA DO CLIENTE</a></span>
-        <span><a href="contato.php">CONTATO</a></span>
+        <span><a href="">ÁREA DO CLIENTE</a></span>
+        <span><a href="">CONTATO</a></span>
 
-</nav>
+</nav> -->
