78a019fad4c57da2c70d754ff5f0ea86f735ec8e
allansouki <allan.souki@hotmail.com>
2019-06-19 00:31:53 -0300

    atualização do carrossel

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" media="screen" href="header.css">
         <link rel="stylesheet" type="text/css" media="screen" href="footer.css">
-        <link rel="stylesheet" type="text/css" media="screen" href="carrossel.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="carr.css">
         <link rel="icon" href="http://www.torabit.com.br/wp-content/uploads/2017/04/cropped-favicon3-2-32x32.png" sizes="32x32">
     </head>
 
@@ -91,9 +91,9 @@
     </section>
    </section>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script type="text/javascirpt" src="carrosel.js"></script>
+    
     </body>
 </html>
