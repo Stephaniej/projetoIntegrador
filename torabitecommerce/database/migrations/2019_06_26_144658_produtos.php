4ba361cb7a7c8b2957d1b5ef57c48f67bba9fd37
Stephanie Jorge <stephanie.jorge@gmail.com>
2019-06-26 11:56:16 -0300

    migrations usuarios e produtos

---
+++ torabitecommerce/database/migrations/2019_06_26_144658_produtos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Produtos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');->unique();
+            $table->string('imagem')->nulable();
+            $table->string('descricao')->nulable();
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
