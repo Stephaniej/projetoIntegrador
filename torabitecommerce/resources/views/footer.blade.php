8b9dd1cd2659b8960e9901a0d8b953127b0999b1
Stephanie Jorge <stephanie.jorge@gmail.com>
2019-07-01 10:17:27 -0300

    ajustes home, footer e carrossel

--- torabitecommerce/resources/views/footer.blade.php
+++ torabitecommerce/resources/views/footer.blade.php
@@ -1,17 +1,13 @@
-
-
 <footer id="rodape" >
   <div class="main-footer">
     <div class="txt-footer">
     
-      <h6>Torabit • Monitoramento digital</h6>
-      <h6>&copy; Copyright 2019 • Todos os direitos reservados</h6>
+      <h6 class="h6margin">Torabit • Monitoramento digital</h6>
+      <h6 class="h6margin">&copy; Copyright 2019 • Todos os direitos reservados</h6>
       
-  </div>    
-  <div class="div-img">  
-    <img class="img-logo-tora" src="{{asset('imgs/imagens_cortadas/torabit.png')}}" >
-  </div>
+    </div>    
+    <div class="div-img">  
+      <img class="img-logo-tora" src="{{asset('imgs/imagens_cortadas/torabit.png')}}" >
+    </div>
   </div>
 </footer>
-
-@endsection
